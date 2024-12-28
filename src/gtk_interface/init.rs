834fcf5b8bc64ecd2a9672a363c11f728fc2a7ea
TalesPalma <tales.palma_2022@outlook.com>
2024-12-28 10:52:27 -0300

    Button open folder

--- src/gtk_interface/init.rs
+++ src/gtk_interface/init.rs
@@ -1,21 +1,12 @@
 extern crate gtk;
 
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::process::Command;
 
 use gtk::glib::{MainContext, Propagation};
 use gtk::{prelude::*, Statusbar};
 use gtk::{Button, Entry, Window, WindowType};
 
 use crate::service::service_donwload::baixar_playlist;
-
-enum Status {
-    Ok,
-    NotFound,
-    Progress,
-    Error,
-}
 
 pub fn interface() {
     //init interface
@@ -27,33 +18,58 @@ pub fn interface() {
     window.set_default_size(400, 70);
 
     //button create
-    let button = Button::new();
-    button.set_label("Baixar");
+    let button_donwload = Button::new();
+    button_donwload.set_label("Baixar");
+
+    //button create open folder with musics
+    let button_open_folder = Button::new();
+    button_open_folder.set_label("Abrir pasta das musicas baixadas");
+    button_open_folder.set_sensitive(false);
 
     // Criação da barra de status
     let status_bar = Statusbar::new();
+    status_bar.push(0, "Bem vindo!");
     // entry create
     let text_entry = Entry::new();
 
     // create layout box
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10);
     vbox.pack_start(&text_entry, true, false, 0);
-    vbox.pack_start(&button, true, false, 0);
+    vbox.pack_start(&button_donwload, true, false, 0);
     vbox.pack_start(&status_bar, true, false, 0);
+    vbox.pack_end(&button_open_folder, true, false, 0);
 
     // add layout to window
     window.add(&vbox);
 
+    // click button open folder
+    button_open_folder.connect_clicked(move |_| {
+        let url_text = "/home/talespalma/Musicas/";
+        if let Err(e) = Command::new("xdg-open").arg(url_text).spawn() {
+            eprintln!("Erro ao abrir pasta {}", e)
+        }
+    });
+
     // click button baixar
-    button.connect_clicked(move |_| {
+    button_donwload.connect_clicked(move |e| {
         let url_text = text_entry.text().to_string();
         println!("URL {}", url_text);
 
         let value_bar = status_bar.clone();
-        MainContext::default().spawn_local(async move {
+        let button_open_folder_clone = button_open_folder.clone();
+
+        e.set_sensitive(false);
+
+        let context = MainContext::default();
+        context.spawn_local(async move {
             match chamar_funcao_baixar_musica(url_text).await {
-                Ok(_) => modificar_status_bar(Status::Ok, value_bar),
-                Err(_) => modificar_status_bar(Status::Error, value_bar),
+                Ok(_) => {
+                    modificar_status_bar("Sucesso ao baixar", value_bar);
+                    button_open_folder_clone.set_sensitive(true);
+                }
+                Err(_) => {
+                    modificar_status_bar("Erro ao baixar: Tente novamente", value_bar);
+                }
             }
         });
     });
@@ -76,11 +92,6 @@ async fn chamar_funcao_baixar_musica(url: String) -> Result<(), ()> {
     }
 }
 
-fn modificar_status_bar(status: Status, status_bar: Statusbar) {
-    match status {
-        Status::Ok => status_bar.push(0, "Baixado com sucesso!"),
-        Status::NotFound => status_bar.push(0, "URL não encontrada!"),
-        Status::Progress => status_bar.push(0, "Baixando..."),
-        Status::Error => status_bar.push(0, "Erro ao baixar!"),
-    };
+fn modificar_status_bar(status_msg: &str, status_bar: Statusbar) {
+    status_bar.push(0, status_msg);
 }
